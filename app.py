06d782674d605c4a564c2b4b6652b9af08d5140d
mitushiro murata <addressnasio@gmail.com>
2020-09-11 08:19:40 +0900

    change

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # coding: utf-8
-from flask import *
+from flask import Flask, render_template
 app = Flask(__name__)
+import test
 
 '''
 @app.route('/')
@@ -12,11 +13,17 @@ def main():
 def main_page():
     return render_template("mainpage.html")
 
-
+"""
 @app.route("/<name>", methods=["GET", "POST"])
 def namepage(name):
     return render_template("name.html")
-
+"""
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="8000")
+
+
+@app.route('/test')
+def get():
+    # ↓　実行したいファイルの関数
+    return test.hello()
