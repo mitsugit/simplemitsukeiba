7ac4fae2aa77d48a1170de9e5f6455b097f875fe
mitushiro murata <addressnasio@gmail.com>
2020-09-12 09:44:47 +0900

    calculation logic added

--- pyfolder/keisan.py
+++ pyfolder/keisan.py
@@ -9,6 +9,8 @@ import decimal
 def sum_cal():
     total = 0
     
+    budget = request.form["budget"]
+
     odds1 = request.form["keyword1"]
     odds2 = request.form["keyword2"]
     odds3 = request.form["keyword3"]
@@ -17,10 +19,73 @@ def sum_cal():
 
     odds_list = [float(i) for i in [odds1,odds2,odds3]]
 
-    total = float(odds1) + float(odds2) + float(odds3)
+    odds_dict={}
+    for num,i in enumerate(odds_list):
+        num=num+1
+        key = "odds"+str(num)
+        odds_dict[key]=i
+    
+    ################### 合成オッズの計算 ###################
+    goukei = 0
+    pre_gousei_odds = 0
 
-    total = str(total)
+    n = 1
 
-    result = {'odds1':odds1,'odds2':odds2,'odds3':odds3,'total':total}
+    for i in range(len(odds_list)):
+        goukei +=  1 / odds_list[i]    
+
+    pre_gousei_odds = 1 / goukei
+
+    gousei_odds = math.floor(pre_gousei_odds*10**n)/(10**n)
+
+
+    ################### 資金分配の計算 ###################
+
+    each_bet = []
+
+    C = 0
+
+    # リスト内の最小値を取得し、１００を乗じる
+    A = min(odds_list) * 100
+
+
+    budget = int(budget)
+
+    # オッズの最小値を各オッズで割ったものをリスト化する
+    B_list = [A / i for i in odds_list]
+
+    # 小数切り捨て
+    B_list = list(map(lambda x: math.floor(x), B_list))
+
+    #print(B_list)
+
+    C = sum(B_list)
+
+    #print("C", C)
+    ratio = [B_list[i] / C * 100 for i in range(0, len(B_list))]
+
+    ratio = np.round(ratio, 1)
+
+    each_bet = [int(np.round(budget * i/100,-2)) for i in ratio]
+
+    print("each_bet:   ",each_bet)
+
+
+################### 利益計算 ###################
+
+    return_list = [xx * yy for (xx,yy) in zip(each_bet,odds_list)]
+    return_list = list(map(lambda x: math.floor(x), return_list))
+
+    print("return_list:   ",return_list)
+
+    total_bet = sum(each_bet)
+
+    print("合計ベット金額:  ",total_bet,"円")
+
+    profit_list = [x-total_bet for x in return_list]
+
+
+    result = {'odds_dict':odds_dict,'each_bet':each_bet,'total_bet':total_bet,\
+              'return_list':return_list,'budget':budget}
     return result
 
